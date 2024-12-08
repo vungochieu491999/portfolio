9ef9b0bd2e73fb5d4e1f810ffb19a2cae0235ffc
vungochieu491999 <vungochieu491999@gmail.com>
2024-12-08 21:31:25 +0700

    update skills

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
 const hamburger = document.getElementById('hamburger');
 const menu = document.querySelector('.menu');
-
 hamburger.addEventListener('click', function () {
-    const hamIcon = this.querySelector('.hamburger-icon');
-    const crossIcon = this.querySelector('.cross-icon');
-    if (hamIcon.style.display === "none") {
-        hamIcon.style.display = "inline-block"
+    const mobileMenu = this.querySelector('.mobile-menu-btn');
+    if (mobileMenu.style.display === "none") {
+        mobileMenu.style.display = "inline-block"
         menu.style.display = "none"
-        crossIcon.style.display = "none"
     }
-    else {
-        crossIcon.style.display = "inline-block"
-        hamIcon.style.display = "none"
+    else if(menu.style.display === "none") {
         menu.style.display = "block"
+    } else {
+        menu.style.display = "none"
     }
+});
+
+function showSection(sectionId) {
+// Hide all sections
+const sections = document.querySelectorAll('.section');
+sections.forEach(section => {
+    section.style.display = 'none';
+});
+
+// Show the selected section
+const activeSection = document.getElementById(sectionId);
+if (activeSection) {
+    activeSection.style.display = 'block';
+}
+
+const hamburger = document.getElementById('hamburger');
+const hamburgerDisplay = window.getComputedStyle(hamburger).display;
+if (hamburgerDisplay === "none") {
+    menu.style.display = "block"
+} else {
+    menu.style.display = "none"
+}
+}
+
+// Set default section to display
+document.addEventListener('DOMContentLoaded', () => {
+showSection('home');
 });
